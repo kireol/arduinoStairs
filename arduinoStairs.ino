523db19326490fb6db2c2743b36e61ac64b907e0
Chris Parent <chris@motorcitysoftware.com>
2016-01-26 09:46:58 -0500

    CP - removed unused variables. refactored and renamed a lot

--- arduinoStairs.ino
+++ arduinoStairs.ino
@@ -12,42 +12,30 @@
 
 int PIR1InputPin = 2;
 int PIR2InputPin = 3;
-int val1 = 0;
-int val2 = 0;
+#define DATA_PIN 4
+#define DATA_PIN2 5
+#define TOTAL_LEDS 13
 
+static const uint32_t onColor = CRGB::AliceBlue;
+#define COLORCHASESPEED 1
+#define PULSEDELAY 1
+#define FADEONSPEED 100
+#define NUMBER_OF_TIMES_TO_CYCLE_PULSE 60
+//#define CLOCK_PIN 8 //green
 #define POTENTIOMETER_PIN A0;
 
-//Tell it how many leds are in the strip.
-#define TOTAL_LEDS 13
-//if you want to turn them on and off as a unit.
-#define LEDS_IN_A_GROUP 1
-//color you want to use when the LED is on
-//static const uint32_t onColor = 0xF7F76F;
-static const uint32_t onColor = CRGB::AliceBlue;
-static const uint32_t leadColor = CRGB::DeepPink;
-//milliseconds led is off when dark led cycling
-#define offChaseDelay 1
-//milliseconds between turning next led on or off
-#define onAndOffDelay 20
-#define PULSEDELAY 1
-#define FADEONDELAY 100
-//number of times to cycle the entire set with a dark led cycle
-#define cycleOffChaseTimes 60
-
 #define MAX_BRIGHTNESS 255
-
+#define NO_DELAY 0
+#define GOING_UP 1
+#define GOING_DOWN 2
+#define NOT_GOING_UP_OR_DOWN 0
 CRGB leds[TOTAL_LEDS];
-int colors[TOTAL_LEDS];
-//Which pins in your arduino to talk to the LED strip
-#define DATA_PIN 4  //white
-#define DATA_PIN2 5  //white
-//#define CLOCK_PIN 8 //green
-//blue ground
-//red 5v
-int downUp = 0;              // variable to rememer the direction of travel up or down the stairs
-unsigned long timeOut = 60000; // timestamp to remember when the PIR was triggered.
-int alarmValueTop = LOW;    // Variable to hold the PIR status
-int alarmValueBottom = LOW; // Variable to hold the PIR status
+int alarmValueTop = LOW;
+int alarmValueBottom = LOW;
+
+void setLedWithFadedBrightness(const uint32_t &color, int currentStep, int thisLed, int ledToChange);
+
+void setFadingColorsForStrip(const uint32_t &color, const boolean &forward, int currentStep);
 
 void setup() {
     Serial.begin(9600);
@@ -56,7 +44,7 @@ void setup() {
 
     addFastLEDs();
     FastLED.clear();
-    colorChase(CRGB::Black, CRGB::Black, onAndOffDelay, true);
+    colorChase(CRGB::Black, CRGB::Black, NO_DELAY, true);
 }
 
 void loop() {
@@ -82,60 +70,58 @@ void addFastLEDs() {
 }
 
 void checkPIR() {
+    int goingDownOrUp = NOT_GOING_UP_OR_DOWN;
+
     alarmValueTop = digitalRead(PIR1InputPin);
     alarmValueBottom = digitalRead(PIR2InputPin);
 
-    if (alarmValueTop == HIGH && downUp != 2) {
-        //    Serial.println("CHECKING PIRS");
-        downUp = 1;
-        Serial.println("UP");
+    if (alarmValueTop == HIGH && goingDownOrUp != GOING_DOWN) {
+        goingDownOrUp = GOING_UP;
         runCycle(onColor, true);
     }
-    else if (alarmValueBottom == HIGH && downUp != 1) {
-        downUp = 2;
-        Serial.println("DOWN");
+    else if (alarmValueBottom == HIGH && goingDownOrUp != GOING_UP) {
+        goingDownOrUp = GOING_DOWN;
         runCycle(onColor, false);
     }
     else {
-        downUp = 0;
+        goingDownOrUp = NOT_GOING_UP_OR_DOWN;
     }
 }
 
-void runCycle(uint32_t onColor, boolean forward) {
-    timeOut = millis();
+void runCycle(uint32_t onColor, boolean isGoingUp) {
     FastLED.clear();
     FastLED.setBrightness(MAX_BRIGHTNESS);
 
-    //turn strip on
-    fadeAllOn(onColor, forward);
+    //turn strip on fading leds further away from the PIR
+    fadeAllOn(onColor, isGoingUp);
 
-    //chase with black to give pulse effect
-    for (int i = 0; i < cycleOffChaseTimes; i++) {
-        colorChase(onColor, onColor, offChaseDelay, forward);
+    //chase with black to give chase pulse effect
+    for (int i = 0; i < NUMBER_OF_TIMES_TO_CYCLE_PULSE; i++) {
+        colorChase(onColor, onColor, COLORCHASESPEED, isGoingUp);
     }
 
     //turn strip off
-    colorChase(CRGB::Black, CRGB::Black, onAndOffDelay, !forward);
+    colorChase(CRGB::Black, CRGB::Black, COLORCHASESPEED, !isGoingUp);
 }
 
-void colorChase(uint32_t onColor, uint32_t offColor, uint8_t wait, boolean forward) {
+void colorChase(uint32_t onColor, uint32_t offColor, uint8_t wait, boolean isGoingUp) {
     int stepBy = 5;
 
-    if (forward) {
-        for (int ledNumber = 0; ledNumber < TOTAL_LEDS; ledNumber += LEDS_IN_A_GROUP) {
-            for (int faderAmount = 255; faderAmount >= 0; faderAmount -= stepBy) {
+    if (isGoingUp) {
+        for (int ledNumber = 0; ledNumber < TOTAL_LEDS; ledNumber += 1) {
+            for (int faderAmount = MAX_BRIGHTNESS; faderAmount >= 0; faderAmount -= stepBy) {
                 showGroup(ledNumber, onColor, faderAmount);
             }
-            for (int faderAmount = 0; faderAmount <= 255; faderAmount += stepBy) {
+            for (int faderAmount = 0; faderAmount <= MAX_BRIGHTNESS; faderAmount += stepBy) {
                 showGroup(ledNumber, onColor, faderAmount);
             }
         }
     } else {
-        for (int ledNumber = TOTAL_LEDS; ledNumber >= 0; ledNumber -= LEDS_IN_A_GROUP) {
-            for (int faderAmount = 255; faderAmount >= 0; faderAmount -= stepBy) {
+        for (int ledNumber = TOTAL_LEDS; ledNumber >= 0; ledNumber -= 1) {
+            for (int faderAmount = MAX_BRIGHTNESS; faderAmount >= 0; faderAmount -= stepBy) {
                 showGroup(ledNumber, onColor, faderAmount);
             }
-            for (int faderAmount = 0; faderAmount <= 255; faderAmount += stepBy) {
+            for (int faderAmount = 0; faderAmount <= MAX_BRIGHTNESS; faderAmount += stepBy) {
                 showGroup(ledNumber, onColor, faderAmount);
             }
         }
@@ -148,24 +134,32 @@ void fadeAllOn(uint32_t color, boolean forward) {
     int maxStep = 256;
 
     for (int currentStep = 0; currentStep < maxStep; currentStep++) {
-        for (int thisLed = 0; thisLed < TOTAL_LEDS; thisLed++) {
-            int ledToChange = thisLed;
-            if (!forward) {
-                ledToChange = TOTAL_LEDS - 1 - thisLed;
-            }
-            leds[ledToChange] = color;
-            int brightnessToUse = currentStep - (thisLed / TOTAL_LEDS) * currentStep;
-            if (brightnessToUse < 0) {
-                brightnessToUse = 0;
-            }
-            else if (brightnessToUse > MAX_BRIGHTNESS) {
-                brightnessToUse = MAX_BRIGHTNESS;
-            }
-            leds[ledToChange] %= brightnessToUse;
-        }
+        setFadingColorsForStrip(color, forward, currentStep);
         FastLED.show();
-        delay(FADEONDELAY);
+        delay(FADEONSPEED);
     }
+}
+
+void setFadingColorsForStrip(const uint32_t &color, const boolean &forward, int currentStep) {
+    for (int thisLed = 0; thisLed < TOTAL_LEDS; thisLed++) {
+        int ledToChange = thisLed;
+        if (!forward) {
+            ledToChange = TOTAL_LEDS - 1 - thisLed;
+        }
+        setLedWithFadedBrightness(color, currentStep, thisLed, ledToChange);
+    }
+}
+
+void setLedWithFadedBrightness(const uint32_t &color, int currentStep, int thisLed, int ledToChange) {
+    leds[ledToChange] = color;
+    int brightnessToUse = currentStep - (thisLed / TOTAL_LEDS) * currentStep;
+    if (brightnessToUse < 0) {
+        brightnessToUse = 0;
+    }
+    else if (brightnessToUse > MAX_BRIGHTNESS) {
+        brightnessToUse = MAX_BRIGHTNESS;
+    }
+    leds[ledToChange] %= brightnessToUse;
 }
 
 void showGroup(int ledNumber, uint32_t color, int faderPercent) {
